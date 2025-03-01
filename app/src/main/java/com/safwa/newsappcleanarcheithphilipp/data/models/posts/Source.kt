d729e5444226bbef25b143d02aaaaeb4b32c1492
ma7modGamel <mahmoudgamel10@gmail.com>
2025-03-01 12:06:49 +0300

    create source model to converter

---
+++ app/src/main/java/com/safwa/newsappcleanarcheithphilipp/data/models/posts/Source.kt
@@ -0,0 +1,11 @@
+package com.safwa.newsappcleanarcheithphilipp.data.models.posts
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("name")
+    val name: String
+)
