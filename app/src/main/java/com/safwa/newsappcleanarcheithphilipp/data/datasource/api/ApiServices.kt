3adc23b208e2468c52334de242b3c4774ebbac83
ma7modGamel <mahmoudgamel10@gmail.com>
2025-03-01 10:50:10 +0300

    create get Method

--- app/src/main/java/com/safwa/newsappcleanarcheithphilipp/data/datasource/api/ApiServices.kt
+++ app/src/main/java/com/safwa/newsappcleanarcheithphilipp/data/datasource/api/ApiServices.kt
@@ -4,8 +4,13 @@ package com.safwa.newsappcleanarcheithphilipp.data.datasource.api
 import com.safwa.newsappcleanarcheithphilipp.data.models.Posts
 import kotlinx.coroutines.flow.StateFlow
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiServices {
     @GET("posts")
-    fun getPosts(): StateFlow<List<Posts>>
+    suspend fun getPosts(
+        @Query("q") text: Int ,
+        @Query("sortBy") sortBy: String,
+        @Query("apiKey") apiKey: String,
+    ): StateFlow<List<Posts>>
 }
--- app/src/main/java/com/safwa/newsappcleanarcheithphilipp/data/datasource/api/RetrofitConstant.kt
+++ app/src/main/java/com/safwa/newsappcleanarcheithphilipp/data/datasource/api/RetrofitConstant.kt
@@ -1,5 +1,5 @@
 package com.safwa.newsappcleanarcheithphilipp.data.datasource.api
 
 object RetrofitConstant {
-    const val URL: String =""
+    const val URL: String ="https://newsapi.org/v2/everything"
 }
